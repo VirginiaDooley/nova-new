9d9afa701e0c8d2b8fcd4dbd56d4f769bdfe1337
Virginia Dooley <vcdooley@gmail.com>
2019-03-17 10:00:37 +0000

    controller actions index new create and strong params for Org

--- app/controllers/organisations_controller.rb
+++ app/controllers/organisations_controller.rb
@@ -1,2 +1,32 @@
 class OrganisationsController < ApplicationController
+
+    def index
+      @organisations = Organisation.all
+    end
+
+    def show
+      @organisation = Organisation.find(params[:id])
+    end
+
+    def new
+      @organisation = Organisation.new
+    end
+
+    def create
+      @organisation = Organisation.create(org_params)
+
+      redirect_to @organisation
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
+
+    private
+
+    def org_params
+      params.require(:organisation).permit(:name)
+    end
 end
