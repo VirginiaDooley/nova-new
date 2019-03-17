e17753b6bc4b367ba5455e822b0b9ad70db59604
Virginia Dooley <vcdooley@gmail.com>
2019-03-17 09:53:39 +0000

    notes file

---
+++ notes.rb
@@ -0,0 +1,15 @@
+organisation
+have many programmes
+has many clients through programmes
+
+
+programmes
+belong to organisation
+has many outcomes through clients
+
+clients
+belongs to programmes
+has many outcomes
+
+outcomes
+belongs to clients
