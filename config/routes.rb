56891b264bf2c1d9399bc589d63090560617eedd
Virginia Dooley <vcdooley@gmail.com>
2019-03-17 13:26:51 +0000

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :organisations
-  resources :programmes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'programmes/create' 
+
+  resources :organisations do
+    resources :programmes
+  end
+
+  root 'organisations#index'
+
 end
