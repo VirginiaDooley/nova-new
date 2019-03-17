34728b7b678ced2edf033faab5f201a30c6996a4
Virginia Dooley <vcdooley@gmail.com>
2019-03-17 12:31:40 +0000

    fix error in tables

--- db/migrate/20190317121913_create_programmes.rb
+++ db/migrate/20190317121913_create_programmes.rb
@@ -2,7 +2,9 @@ class CreateProgrammes < ActiveRecord::Migration[5.2]
   def change
     create_table :programmes do |t|
       t.string :title
-      t.integer :org_id
+      t.integer :organisation_id
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_17_105544) do
+ActiveRecord::Schema.define(version: 2019_03_17_121913) do
 
   create_table "organisations", force: :cascade do |t|
     t.string "name"
@@ -20,7 +20,9 @@ ActiveRecord::Schema.define(version: 2019_03_17_105544) do
 
   create_table "programmes", force: :cascade do |t|
     t.string "title"
-    t.integer "org_id"
+    t.integer "organisation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
