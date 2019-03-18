165cf76ce867c07c68595399f0629a410532a330
Virginia Dooley <vcdooley@gmail.com>
2019-03-18 10:58:59 +0000

    fix show action

--- app/controllers/programmes_controller.rb
+++ app/controllers/programmes_controller.rb
@@ -10,19 +10,35 @@ class ProgrammesController < ApplicationController
 
   def create
     @organisation = Organisation.find(params[:organisation_id])
+    # raise @organisation.inspect
     @programme = @organisation.programmes.build(programme_params)
+    # raise @programme.inspect
     if @programme.save
-      redirect_to organisation_programme_path(@programme, @organisation)
+      redirect_to organisation_path(@organisation)
     else
       render "organisations/show"
     end
   end
 
-  private
-
-  def programme_params
-    params.require(:programme).permit(:title, :organisation_id)
+  def edit
+    @programme = Programme.find(params[:id])
   end
 
+  def update
+    @programme = Programme.find(params[:id])
+    @programme.update(programme_params)
+    redirect_to organisation_programme_path(@organisation)
+  end
+
+  def destroy
+    @programme = Programme.find(params[:id]).destroy
+    redirect_to organisation_programmes_path
+  end
+
+  private
+
+    def programme_params
+      params.require(:programme).permit(:title)
+    end
 
 end
