16c03c536b4ab4e5a19ce6b79f7ccc096ae05d48
Virginia Dooley <vcdooley@gmail.com>
2019-03-19 10:10:04 +0000

    client controller actions

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,27 @@
 class ClientsController < ApplicationController
+
+  def index
+    @clients = Client.all
+  end
+
+  def show
+    @client = Client.find(params[:id])
+  end
+
+  def create
+    @programme = Programme.find(params[:programme_id])
+    @client = @programme.clients.build(client_params)
+    if @client.save
+      redirect_to programme_path(@programme)
+    else
+      render "programmes/show"
+    end
+  end
+
+  private
+
+    def client_params
+      params.require(:client).permit(:first_name, :last_name, :email, :phone, :address1, :address2, :city, :post_code, :country)
+    end
+
 end
