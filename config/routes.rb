ab33f45dedac5d7c2f170175987158bf18ab4c92
Virginia Dooley <vcdooley@gmail.com>
2019-03-19 11:01:46 +0000

    nested resources for programmes and clients

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
     resources :programmes
   end
 
-  resources :programmes
+  resources :programmes do
+    resources :clients
+  end  
 
   root 'organisations#index'
 
