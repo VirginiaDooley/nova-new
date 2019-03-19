0ef6105f482864420879cd0924ba1f556170429d
Virginia Dooley <vcdooley@gmail.com>
2019-03-19 10:01:10 +0000

    attributes for clients table

--- db/migrate/20190319094736_create_clients.rb
+++ db/migrate/20190319094736_create_clients.rb
@@ -1,6 +1,15 @@
 class CreateClients < ActiveRecord::Migration[5.2]
   def change
     create_table :clients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :city
+      t.string :post_code
+      t.string :address1
+      t.string :address2
+      t.string :country
 
       t.timestamps
     end
