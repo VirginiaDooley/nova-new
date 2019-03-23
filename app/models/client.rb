35bf571b0d3669c45d2070772c6c8974e0aa3bcc
Virginia Dooley <vcdooley@gmail.com>
2019-03-23 13:47:31 +0000

    custom method for client full name

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,8 @@ class Client < ApplicationRecord
   has_and_belongs_to_many :programmes
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def full_name
+    self.first_name + self.last_name
+  end
 end
