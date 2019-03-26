ac3cc9c5570c4a466b8be14055b251119ff8c6bc
Virginia Dooley <vcdooley@gmail.com>
2019-03-26 22:09:43 +0000

    notes update

--- notes.rb
+++ notes.rb
@@ -16,5 +16,24 @@ belongs to clients
 
 edit and delete programmes
 
+Install Devise
+Org has_many Users
+User belongs to Org
+
 CRUD programme clients
 to create client status you can use this as an example Rails TODOMVC - Lecture 3: Marking Items Complete and Incomplete around 10 mins
+
+A User can
+CRUD an account
+
+Add a foreign key to a User model
+Add website to Org
+
+When a User creates an account (User controller, create action)
+scan org website where domains match email domain
+
+Create a User Org
+check if org_id is empty
+scan org website where domains match email domain
+
+Docker & Wordpress?? 
