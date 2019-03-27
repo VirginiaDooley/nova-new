f0c9685815b68691fcc87fb7fbc65ebe82cd57b2
Virginia Dooley <vcdooley@gmail.com>
2019-03-27 15:02:51 +0000

    user create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,24 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+
+  def new
+    @user = User.new(user_params)
+  end
 
   def create
-  
+    @user = User.new(user_params)
+    if @user.email_domain == @organisation.website_domain
+      @user.organisation.build(user_params)
+      @user.save
+      redirect to new_user_session_path
+    else
+    #present user with option to create an org
+    redirect to new_user_session_path
+    end
   end
 
   def user_params
-
+    params.require(:user).permit(:email, :organisation_id)
   end
+
 end
