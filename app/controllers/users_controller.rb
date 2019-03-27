a514912a0aa8d465dc6f680457e602f33065a849
Virginia Dooley <vcdooley@gmail.com>
2019-03-27 20:32:47 +0000

    remove actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,3 @@
 class UsersController < ApplicationController
-  # before_action :authenticate_user!
-
-  def new
-    @user = User.new(user_params)
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.email_domain == @organisation.website_domain
-      @user.organisation.build(user_params)
-      @user.save
-      redirect to new_user_session_path
-    else
-    #present user with option to create an org
-    redirect to new_user_session_path
-    end
-  end
-
-  def user_params
-    params.require(:user).permit(:email, :organisation_id)
-  end
-
+  before_action :authenticate_user!
 end
