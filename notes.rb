d2723c8e3704b6e0da98fe491da34ed33bcc16e7
Virginia Dooley <vcdooley@gmail.com>
2019-03-27 09:05:27 +0000

    notes update

--- notes.rb
+++ notes.rb
@@ -16,9 +16,9 @@ belongs to clients
 
 edit and delete programmes
 
-Install Devise
-Org has_many Users
-User belongs to Org
+x Install Devise
+x Org has_many Users
+x User belongs to Org
 
 CRUD programme clients
 to create client status you can use this as an example Rails TODOMVC - Lecture 3: Marking Items Complete and Incomplete around 10 mins
@@ -26,8 +26,8 @@ to create client status you can use this as an example Rails TODOMVC - Lecture 3
 A User can
 CRUD an account
 
-Add a foreign key to a User model
-Add website to Org
+x Add a foreign key to a User model
+x Add website to Org
 
 When a User creates an account (User controller, create action)
 scan org website where domains match email domain
@@ -35,5 +35,20 @@ scan org website where domains match email domain
 Create a User Org
 check if org_id is empty
 scan org website where domains match email domain
+present create a new org (i think this fulfills writable attribute?)
 
-Docker & Wordpress?? 
+
+ideas for ...
+def create 
+  @user = User.new(user_params)
+  @organisation.id = @user.organisation_id
+  domain = regex split after @ of email address
+  @user.email.scan.includes?
+
+  Organisation.all.detect{ |o| o.website.includes?(@user.email)}
+
+  SELECT * FROM organisations WHERE (organisations.website.includes == @user.email)
+
+end
+
+Docker & Wordpress??
