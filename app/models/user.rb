92ab50e9733773e9c69964f6cba655cbb813b15e
Virginia Dooley <vcdooley@gmail.com>
2019-03-27 20:33:13 +0000

    remove association with organisation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :organisation
 
   def email_domain
     self.email_field.split("@").last
