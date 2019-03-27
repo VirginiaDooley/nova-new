288902bdc73e8cfd44bcacde2c76ce8e87a210d3
Virginia Dooley <vcdooley@gmail.com>
2019-03-27 15:04:13 +0000

    validate for website presence

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,6 +1,7 @@
 class Organisation < ApplicationRecord
   has_many :programmes
   validates :name, presence: true
+  validates :website, presence: true
 
   def website_domain
     self.website.split("www.").last
