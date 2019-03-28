46e2bf9c841dfbd1a607f00141c767816263d573
Virginia Dooley <vcdooley@gmail.com>
2019-03-28 12:58:33 +0000

    remove comments

--- db/migrate/20190319113421_create_clients_programmes_join_table.rb
+++ db/migrate/20190319113421_create_clients_programmes_join_table.rb
@@ -1,12 +1,5 @@
 class CreateClientsProgrammesJoinTable < ActiveRecord::Migration[5.2]
   def change
-    # This is enough; you don't need to worry about order
     create_join_table :clients, :programmes
-  #
-  #   # If you want to add an index for faster querying through this join:
-  #   create_join_table :clients, :programmes do |t|
-  #     t.index :client_id
-  #     t.index :programme_id
-  #   end
   end
 end
