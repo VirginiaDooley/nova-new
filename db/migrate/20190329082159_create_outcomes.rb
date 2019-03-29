232f04fd77487fcdfe0ddb9beb50d7499d4939e9
Virginia Dooley <vcdooley@gmail.com>
2019-03-29 08:43:18 +0000

    add description and grade to outcome table

--- db/migrate/20190329082159_create_outcomes.rb
+++ db/migrate/20190329082159_create_outcomes.rb
@@ -1,6 +1,8 @@
 class CreateOutcomes < ActiveRecord::Migration[5.2]
   def change
     create_table :outcomes do |t|
+      t.text :description
+      t.integer :grade
 
       t.timestamps
     end
