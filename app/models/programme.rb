654e726f484313f0ed9e7a57850083e07ec8cdac
Virginia Dooley <vcdooley@gmail.com>
2019-03-29 15:46:42 +0000

    associations

--- app/models/programme.rb
+++ app/models/programme.rb
@@ -1,6 +1,6 @@
 class Programme < ApplicationRecord
   belongs_to :organisation
   has_and_belongs_to_many :clients
-  has_many :outcomes, through: :clients 
+  has_many :outcomes, through: :clients
   validates :title, presence: true
 end
