7292fee4b0eef38d2725f3f9e088cd85ef3aff6d
Virginia Dooley <vcdooley@gmail.com>
2019-03-29 15:45:25 +0000

    fix typo

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,9 @@
 class Client < ApplicationRecord
   has_and_belongs_to_many :programmes
+  has_many :outcomes
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, uniqueness: true
 
   def full_name
     self.first_name + " " + self.last_name
