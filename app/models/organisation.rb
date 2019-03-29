843a00c82a6074a8d87054f1ea091828c8a0556b
Virginia Dooley <vcdooley@gmail.com>
2019-03-29 15:45:47 +0000

    move validations to model

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -2,6 +2,8 @@ class Organisation < ApplicationRecord
   has_many :programmes
   validates :name, presence: true
   validates :website, presence: true
+  validates :name, uniqueness: true
+  validates :website, uniqueness: true 
 
   def website_domain
     self.website.split("www.").last
