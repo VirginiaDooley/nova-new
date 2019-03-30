6590c2f2710a86d7e22a2ab99205af0767f2ed4d
Virginia Dooley <vcdooley@gmail.com>
2019-03-30 18:10:11 +0000

    ominauth migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_29_094237) do
+ActiveRecord::Schema.define(version: 2019_03_30_180249) do
 
   create_table "clients", force: :cascade do |t|
     t.string "first_name"
@@ -62,6 +62,12 @@ ActiveRecord::Schema.define(version: 2019_03_29_094237) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "token"
+    t.integer "expires_at"
+    t.boolean "expires"
+    t.string "refresh_token"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
