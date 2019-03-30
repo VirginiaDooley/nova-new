159c9027132992db4ca263e08eb5b3d5d113c792
Virginia Dooley <vcdooley@gmail.com>
2019-03-30 17:34:52 +0000

    migration fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  def email_domain
-    self.email_field.split("@").last
-  end
 end
