40b09bcacbb744574ed0ae6878ccc73f4f4e8399
Virginia Dooley <vcdooley@gmail.com>
2019-04-02 08:07:39 +0100

    test options method

--- app/models/clients_programme.rb
+++ app/models/clients_programme.rb
@@ -1,4 +1,9 @@
 class ClientsProgramme < ApplicationRecord
   belongs_to :client
-  belongs_to :programme 
+  belongs_to :programme
+
+  def status_options
+    s = ["interviewed", "enroled", "due for assessment", "graduated", "volunteering", "employed"]
+  end
+
 end
