39d4175560b7c7512db7fa7658642ff66adc9062
Virginia Dooley <vcdooley@gmail.com>
2019-04-02 08:09:52 +0100

    link to create new outcome

--- app/models/clients_programme.rb
+++ app/models/clients_programme.rb
@@ -2,8 +2,8 @@ class ClientsProgramme < ApplicationRecord
   belongs_to :client
   belongs_to :programme
 
-  def status_options
-    s = ["interviewed", "enroled", "due for assessment", "graduated", "volunteering", "employed"]
-  end
+  # def status_options
+  #   s = ["interviewed", "enroled", "due for assessment", "graduated", "volunteering", "employed"]
+  # end
 
 end
