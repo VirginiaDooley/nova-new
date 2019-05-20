d4d7e16f36f28f512b38acfd0fcace8bc3f51e67
Virginia Dooley <vcdooley@gmail.com>
2019-05-20 20:00:41 +0100

    class constructor and form

--- app/assets/javascripts/organisations.js
+++ app/assets/javascripts/organisations.js
@@ -12,3 +12,23 @@ function showNewOrgForm(){
     document.querySelector('#new-org-form-div').innerHTML = newOrgForm;
   });
 }
+
+// class constructor to make instance of an organisation
+class Organisation {
+	constructor(obj) {
+		this.id = obj.id
+		this.name= obj.name
+		this.website = obj.website
+	}
+
+	static newOrgForm() {
+		return (`
+		<strong>New Organisation</strong>
+			<form>
+				<input id='org-form' type='text' name='name'></input><br>
+				<input type='text' name='website'></input><br>
+				<input id='submit' type='submit'/>
+			</form>
+		`)
+	}
+}
