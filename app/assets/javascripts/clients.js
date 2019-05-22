611c4b9a8729bcc426d3fb2a26a546fd810147ae
Virginia Dooley <vcdooley@gmail.com>
2019-05-22 15:32:19 +0100

    rename file and get document ready for new function

---
+++ app/assets/javascripts/clients.js
@@ -0,0 +1,14 @@
+$(function(){
+  console.log("clients.js is loaded");
+});
+
+
+// write a function that on
+// click will render the contact details
+// of one client on a show page
+
+function showContactDetails() {
+
+
+
+}
