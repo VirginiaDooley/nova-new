0d351020386f83d3958e49e71ab8986c8395f2d1
Virginia Dooley <vcdooley@gmail.com>
2019-05-27 12:37:10 +0100

    refactor to render client list w JS

--- app/assets/javascripts/programmes.js
+++ app/assets/javascripts/programmes.js
@@ -8,5 +8,37 @@ function showProgrammeClients(){
     event.preventDefault()
     console.log("link works")
 
+    let id = parseInt($(".programme-id").attr("data-id"))
+
+    let url = "/programmes/" + id
+    $.ajax({
+      url: url,
+      method: 'get',
+      dataType: 'json',
+      success: function(data){
+        console.log("the data is: ", data);
+
+        let clients = data.clients
+        console.log(clients);
+
+        let programme_clients = clients.map(client => {
+
+        return (`
+          <div id="programme-clients">
+          <li>${client.first_name}  ${client.last_name}</li>
+          </div>
+        `);
+        debugger
+      }).join('');
+        document.getElementById('programme-clients').innerHTML = programme_clients
+
+    }});
   });
 }
+
+//
+// function clientNamesHTML(programme_clients) {
+//   debugger
+//
+//
+// }
