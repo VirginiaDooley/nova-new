dcd3392f9c55712e6d1858e168adf90d0278bdc1
Virginia Dooley <vcdooley@gmail.com>
2019-05-31 13:21:08 +0100

    no change

--- app/serializers/programme_serializer.rb
+++ app/serializers/programme_serializer.rb
@@ -1,7 +1,5 @@
 class ProgrammeSerializer < ActiveModel::Serializer
   attributes :id, :title
-  belongs_to :organisation
-  has_many :clients_programmes
-  has_many :clients, through: :clients_programmes
-  has_many :outcomes, through: :clients
+  has_many :clients
+  has_many :outcomes
 end
