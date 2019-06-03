70bd8f9f57a6e54465af31a570d673a74fa60193
Virginia Dooley <vcdooley@gmail.com>
2019-06-03 10:38:22 +0100

    iterate on client array for a bulleted list

--- app/assets/javascripts/programmes.js
+++ app/assets/javascripts/programmes.js
@@ -8,18 +8,20 @@ function getProgrammeClients(){
   $("a.programme-link").on('click', function() {
 
     let programme_id = $(this).data("id");
-    let programme_url = "/programmes/" + programme_id + "/clients"
+    let programme_url = "/programmes/" + programme_id + "/clients" + ".json"
 
     $.get(programme_url, function(data){
-      console.log('this is your data', data);
-      // need to format this
-      const clientObj = data.map(object => object.first_name + " " + object.last_name)
-      showProgrammeClients(programme_id, clientObj)
-
+      console.log("this is your programme data", data)
+      const clientArr = data.map(object => object["first_name"] + " " + object["last_name"])
+      showProgrammeClients(programme_id, clientArr)
     });
   });
 }
 
-function showProgrammeClients(programme_id, clientObj){
-  $('.programme-' + programme_id + '-clients').text(clientObj)
+function showProgrammeClients(programme_id, clientArr){
+  let text = "<ul>";
+
+  for (i = 0; i < clientArr.length; i++) {text += "<li>" + clientArr[i] + "</li>";}
+  text += "</ul>";
+  $('.programme-' + programme_id + '-clients').append(text)
 }
