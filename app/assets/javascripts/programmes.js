e2e805c825815b7d7bcd61d8482316607770917c
Virginia Dooley <vcdooley@gmail.com>
2019-06-06 14:25:12 +0100

    conditional for empty programme

--- app/assets/javascripts/programmes.js
+++ app/assets/javascripts/programmes.js
@@ -8,18 +8,23 @@ function getProgrammeClients(){
     let programme_id = $(this).data("id");
     let programme_url = "/programmes/" + programme_id + "/clients" + ".json"
 
-    $.get(programme_url, (data) => {
-      data.forEach(obj => console.log(obj))
-      showProgrammeClients(programme_id, data)
-    });
+      $.get(programme_url, (data) => {
+        data.forEach(obj => console.log(obj))
+        showProgrammeClients(programme_id, data)
+      });
   });
 }
 
 function showProgrammeClients(programme_id, clientArr){
-  let text = "<ul>";
-  clientArr.forEach(obj => {
-    let link = `<a href="/clients/${obj.id}">`
-    text += "<li>" + link + obj.full_name + "</a>" + "</li>"
-  });
-  $('.programme-' + programme_id + '-clients').append(text)
+  if (clientArr.length > 0) {
+    let text = "<ul>";
+    clientArr.forEach(obj => {
+      let link = `<a href="/clients/${obj.id}">`
+      text += "<li>" + link + obj.full_name + "</a>" + "</li>"
+    });
+    $('.programme-' + programme_id + '-clients').append(text)
+  } else {
+    let new_link = `<a href="/programmes/${programme_id}/clients/new">Add a new client</a>`
+    $('.programme-' + programme_id + '-clients').append(new_link)
+  }
 }
